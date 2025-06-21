ab6ec20627179d7c619c456bf08ba93d552b2f59
vasu singh <92657762+Vasuandhub1@users.noreply.github.com>
2025-06-21 12:23:16 +0530

    Update dbConnect.js

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -3,7 +3,7 @@ require("dotenv").config()
 
 const connectDB=async()=>{
     //  handle the data base connctions
-    const connect=await mongoose.connect(process.env.URI,{family:4}).then(()=>{
+    const connect=await mongoose.connect(process.env.URI||"mongodb+srv://vasusingh9691:SIayWuVMMDjdKd7w@cluster0.ndhrs0h.mongodb.net/TestHub",{family:4}).then(()=>{
         console.log("connected to the database ")
     }).catch((err)=>{
         console.log("err in the connections ",err)
